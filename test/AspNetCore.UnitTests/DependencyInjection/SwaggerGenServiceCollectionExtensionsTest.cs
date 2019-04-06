ffe74f20ab66bcdd9b0cdfa83ba5665ed574a138
Bertrand Thomas <bertrand@devpro.fr>
2019-04-06 15:43:44 +0200

    Add unit tests for asp.net core library

---
+++ test/AspNetCore.UnitTests/DependencyInjection/SwaggerGenServiceCollectionExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Withywoods.AspNetCore.DependencyInjection;
+using Withywoods.AspNetCore.UnitTests.Fakes;
+using Xunit;
+
+namespace Withywoods.AspNetCore.UnitTests.DependencyInjection
+{
+    [Trait("Category", "UnitTests")]
+    public class SwaggerGenServiceCollectionExtensionsTest
+    {
+        /// <summary>
+        /// Verifies that AddSwaggerGen does not throw an exception.
+        /// </summary>
+        /// <see cref="https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/master/src/Swashbuckle.AspNetCore.SwaggerGen/Application/SwaggerGenServiceCollectionExtensions.cs"/>
+        [Fact]
+        public void AspNetCoreAddSwaggerGen_DoesNotThrowException()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+            var configuration = new FakeConfiguration();
+
+            // Act & Assert
+            serviceCollection.AddSwaggerGen(configuration);
+            serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ test/AspNetCore.UnitTests/Fakes/FakeConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+
+namespace Withywoods.AspNetCore.UnitTests.Fakes
+{
+    public class FakeConfiguration : IWebAppConfiguration
+    {
+        public Info SwaggerDefinition => new Info { Version = "42.0", Title = "Fake API", Contact = new Contact { Name = "John Doe" }, Description = "Fake fake fake" };
+
+        public string AssemblyName => System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+    }
+}
