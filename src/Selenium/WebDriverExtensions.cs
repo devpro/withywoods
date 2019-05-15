cfa66a04fafde7aac4a6928285eccdcfa1dcdc7b
Bertrand THOMAS <bertrand@devpro.fr>
2019-05-15 15:07:57 +0200

    Add FindElementNotDisplayed in WebDriverExtensions

--- src/Selenium/WebDriverExtensions.cs
+++ src/Selenium/WebDriverExtensions.cs
@@ -21,5 +21,18 @@ namespace Withywoods.Selenium
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
             return wait.Until(drv => drv.FindElement(by));
         }
+
+        /// <summary>
+        /// Find an element that must be present but not displayed by using a selector and a driver wait.
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="by"></param>
+        /// <param name="timeoutInSeconds"></param>
+        /// <returns></returns>
+        public static IWebElement FindElementNotDisplayed(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(drv => (drv.FindElement(by) != null && !drv.FindElement(by).Displayed) ? drv.FindElement(by) : null);
+        }
     }
 }
