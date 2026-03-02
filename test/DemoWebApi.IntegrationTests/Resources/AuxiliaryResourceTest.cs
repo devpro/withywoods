7e636830f7e003f67ccf0bb2e03725169610c60c
Bertrand THOMAS <bertrand@devpro.fr>
2026-03-03 00:55:05 +0100

    Verify test result

--- test/DemoWebApi.IntegrationTests/Resources/AuxiliaryResourceTest.cs
+++ test/DemoWebApi.IntegrationTests/Resources/AuxiliaryResourceTest.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using AwesomeAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using VerifyXunit;
 using Xunit;
 
 namespace Withywoods.DemoWebApi.IntegrationTests.Resources;
@@ -28,6 +29,38 @@ public class AuxiliaryResourceTest(WebApplicationFactory<Program> factory)
         response.Content.Headers.ContentType?.ToString().Should().Be(expectedContentType);
 
         var result = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
-        if (!string.IsNullOrEmpty(expectedContent)) result.Should().Be(expectedContent);
+        await VerifyResult(result, expectedContentType, expectedContent, "../Snapshots", SnapshotNameFromUrl(url));
+    }
+
+    private static async Task VerifyResult(string result,
+        string expectedContentType,
+        string? expectedContent,
+        string snapshotDirectory,
+        string snapshotName)
+    {
+        if (!string.IsNullOrEmpty(expectedContent))
+        {
+            result.Should().Be(expectedContent);
+            return;
+        }
+
+        if (expectedContentType.StartsWith("application/json"))
+        {
+            await Verifier.VerifyJson(result)
+                .UseDirectory(snapshotDirectory)
+                .UseFileName(snapshotName);
+            return;
+        }
+
+        await Verifier.Verify(result)
+            .UseDirectory(snapshotDirectory)
+            .UseFileName(snapshotName);
+    }
+
+    private static string SnapshotNameFromUrl(string url)
+    {
+        return url
+            .Trim('/')
+            .Split('/', 2)[0];
     }
 }
