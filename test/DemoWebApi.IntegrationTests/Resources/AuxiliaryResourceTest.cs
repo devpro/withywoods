985e16524f503d7ca4324b581da1e70bb126f84c
Bertrand THOMAS <bertrand@devpro.fr>
2026-03-03 00:19:15 +0100

    Make one nice method for auxiliary resource test

---
+++ test/DemoWebApi.IntegrationTests/Resources/AuxiliaryResourceTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using AwesomeAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Withywoods.DemoWebApi.IntegrationTests.Resources;
+
+[Trait("Category", "IntegrationTests")]
+public class AuxiliaryResourceTest(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Trait("Mode", "Readonly")]
+    [Theory]
+    [InlineData("/health", HttpStatusCode.OK, "text/plain", "Healthy")]
+    [InlineData("/scalar", HttpStatusCode.OK, "text/html")]
+    [InlineData("/openapi/v1.json", HttpStatusCode.OK, "application/json; charset=utf-8")]
+    public async Task AuxiliaryResource_Get_ReturnsExpectedResponse(string url,
+        HttpStatusCode expectedStatus,
+        string expectedContentType,
+        string? expectedContent = "")
+    {
+        var client = factory.CreateClient();
+
+        var response = await client.GetAsync(url, TestContext.Current.CancellationToken);
+        response.StatusCode.Should().Be(expectedStatus);
+
+        response.Content.Headers.ContentType?.ToString().Should().Be(expectedContentType);
+
+        var result = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
+        if (!string.IsNullOrEmpty(expectedContent)) result.Should().Be(expectedContent);
+    }
+}
